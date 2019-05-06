fd97987da1231736bd7150f8b04e507f32f5eea8
Harsh Rajat <ProjectHydro@3602652.no-reply.drupal.org>
2019-05-06 08:40:17 +0530

    Git Tweaks

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-var IceProtocol = artifacts.require("IceProtocol");
+var IceProtocol = artifacts.require("Ice");
 module.exports = function(deployer) {
-  deployer.deploy(IceProtocol/*, "0xB536a9b68e7c1D2Fd6b9851Af2F955099B3A59a9"*/);
+  deployer.deploy(IceProtocol); /*, "0xB536a9b68e7c1D2Fd6b9851Af2F955099B3A59a9"*/
 };
